9f6d541286e4f362518128fe77ab4469c08d0e55
Greg Smith <thinkable@thinkable.us>
2020-02-27 00:00:02 -0500

    automatic commit

--- test/test-glsmaths.js
+++ test/test-glsmaths.js
@@ -1,21 +1,23 @@
 var test = require('unit.js');
 var maths = require('../js/index').maths;
-var log = require('../js/log');
+var { log } = require('../js/log');
 
 describe('testing gls.maths', function () {
     it('generate random numbers', function () {
-        let numbers = [0,0,0,0,0];
-        let samples = 10000;
-        let max = 6;
-        let error = 1;
-        for(let i=0; i<samples; i++) {
-            let rnd = maths.random(1,max);
+        let high = 6;
+        let numbers = (new Array(high + 1)).map(i => 0);
+        let samples = 100000;
+        let error = 0.1;
+        for (let i = 0; i < samples; i++) {
+            let rnd = maths.random(1, high);
             numbers[rnd] = numbers[rnd] + 1;
         }
-        test.value(numbers).match(function(it) {
-            log(it);
-            return true;
-            //return ((samples*(1-error))/max) < it && it < ((samples*(1+error))/6);
+        numbers.slice(1);
+        test.value(numbers).matchEach(function (it) {
+            let min = (samples * (1 - error)) / high;
+            let max = (samples * (1 + error)) / high;
+            log(`${min} ${it} ${max}`);
+            return min < it && it < max;
         });
     });
 });
