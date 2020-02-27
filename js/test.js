2899242424fb54e8fc689f0b42ededc10e7e2107
Greg Smith <thinkable@thinkable.us>
2020-02-27 00:07:07 -0500

    automatic push

--- js/test.js
+++ js/test.js
@@ -2,3 +2,8 @@ const gls = require("../js/index");
 
 let out = gls.files.readJSON('./test/glsfiles-test04.txt');
 console.log(out);
+
+let numbers = 6;
+let foo = new Array(numbers);
+foo = foo.map(i => {return 0});
+console.log(foo);
--- test/test-glsmaths.js
+++ test/test-glsmaths.js
@@ -5,17 +5,19 @@ var { log } = require('../js/log');
 describe('testing gls.maths', function () {
     it('generate random numbers', function () {
         let high = 6;
-        let numbers = (new Array(high + 1)).map(i => 0);
+        let numbers = new Array(high);
+        numbers = numbers.map((value, i) => log(i));
         let samples = 100000;
         let error = 0.1;
         for (let i = 0; i < samples; i++) {
             let rnd = maths.random(1, high);
             numbers[rnd] = numbers[rnd] + 1;
         }
-        numbers.slice(1);
+        numbers = numbers.slice(1);
         test.value(numbers).matchEach(function (it) {
             let min = (samples * (1 - error)) / high;
             let max = (samples * (1 + error)) / high;
+            log(numbers);
             log(`${min} ${it} ${max}`);
             return min < it && it < max;
         });
