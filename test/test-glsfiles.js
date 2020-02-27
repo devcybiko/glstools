80200252480426e97c5c78704e8c7e948486a346
Greg Smith <thinkable@thinkable.us>
2020-02-26 20:10:01 -0500

    automatic commit

--- test/test-glsfiles.js
+++ test/test-glsfiles.js
@@ -12,30 +12,30 @@ Line three #last line`;
     });
     it('can read a file as a list', function () {
         let list = gls.readListFile('test/glsfiles-test01.txt');
-        let expected = [`Line one #comment`, `Line two #blank line`, ``, `Line three #last line`];
+        let expected = [`Line xone #comment`, `Line two #blank line`, ``, `Line three #last line`];
         test.value(list, function (it, i) {
-            list[i] === expected[i]
+            return list[i] === expected[i]
         });
     });
     it('can read a file as a script', function () {
         let list = gls.readScriptFile('test/glsfiles-test01.txt');
-        let expected = [`Line one`, `Line two`, `Line three`];
+        let expected = [`Line xone`, `Line two`, `Line three`];
         test.value(list, function (it, i) {
-            list[i] === expected[i]
+            return list[i] === expected[i]
         });
     });
     it('can read a file as regex', function () {
         let regex = gls.readScriptFile('test/glsfiles-test02.txt');
-        let expected = [/foo/, /bar/, /^.*Greg.*Smith$/];
+        let expected = [/foox/, /bar/, /^.*Greg.*Smith$/];
         test.value(regex, function (it, i) {
-            regex[i] === expected[i]
+            return regex[i] === expected[i]
         });
     });
     it('can read a file as csv', function () {
-        let regex = gls.readCSVFile('test/glsfiles-test03.txt');
+        let csv = gls.readCSVFile('test/glsfiles-test03.txt');
         let expected = [{col1:'a9',col2:'a2',col3:'a3'}, {col1:'b1',col2:'b2',col3:'b3'}, {col1:'c1',col2:'c2',col3:'c3'}];
-        test.value(regex, function (it, i) {
-            regex[i] === expected[i]
+        test.value(csv, function (it, i) {
+            return csv[i] === expected[i]
         });
     });
 });
