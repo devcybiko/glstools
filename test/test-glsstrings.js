20fa5de162a29bb88eefbe87974d47265372eb79
Greg Smith <thinkable@thinkable.us>
2020-02-27 09:26:34 -0500

    adding tests

--- test/test-glsstrings.js
+++ test/test-glsstrings.js
@@ -4,7 +4,15 @@ var {log} = require('../js/log');
 
 describe('testing gls.strings', function () {
     it('can check for restricted characters', function () {
-        let s = strings.checkRestricted("This is a test", ""Tt"");
-        log(s);
+        let zzz = strings.checkRestricted("This is a test", "Zz");
+        test.value(zzz).is(true);
+        let ttt = strings.checkRestricted("This is a test", "Tt");
+        test.value(ttt).is(false);
+    });
+    it('can do caesar cipher', function () {
+        let text = "This is a test";
+        let salad = strings.caesar(text, -12);
+        let unsalad = strings.caesar(salad, 12);
+        test.value(text).is(unsalad);
     });
 });
