6b55fac91fa8be22117ed581826125e3049f9877
Greg Smith <thinkable@thinkable.us>
2020-08-23 14:11:48 -0400

    adding glsaws

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const maths = require('./js/glsmaths');
 const procs = require('./js/glsprocs');
 const chars = require('./js/glschars');
 const debug = require('./js/glsdebug');
+const aws = require('./js/aws');
 const StringBuffer = require('./js/StringBuffer');
 const Tokenizer = require('./js/Tokenizer');
 const List = require('./js/List');
 const Entry = require('./js/Entry');
 
-module.exports = {files, strings, procs, maths, chars, debug, StringBuffer, Tokenizer, List, Entry};
+module.exports = {files, strings, procs, maths, chars, debug, aws, StringBuffer, Tokenizer, List, Entry};
