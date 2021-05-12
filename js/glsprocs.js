a66f745216697c9069cbf05a0144e02458dbd65d
Greg Smith <thinkable@thinkable.us>
2021-05-12 00:00:07 -0400

    automatic commit

--- js/glsprocs.js
+++ js/glsprocs.js
@@ -37,7 +37,7 @@ module.exports = {
     //       --name=default: an option that requires a value and has a default if not specified
     //       --name*=default: an option that is required and has a default value (returns [value,...])
     //       --name-n*=default: an option --name with an alias -n
-    //       -n: a simple option (defined exactly the same as --name, but with only one dash)
+    //       -n: a single-dash option (defined exactly the same as --name, but with only one dash)
     //   fileparms:
     //      name: returns opts.name for the positional parm passed in
     //      name*: required positional parm
@@ -72,28 +72,33 @@ module.exports = {
                 let dashName = words[0];
                 let value = words[1];
                 let option = options[dashName];
+                let name = option && option.name;
+
                 if (!option) {
                     results._errors.push(`Unknown option: ${dashName}`);
                     continue;
                 }
-                if (option.valueRequired) {
-                    if (hasEquals) {
-                        value = value; // nop
-                    } else {
-                        value = option.defaultValue;
-                    }
-                } else {
+                if (option.isAtomic) {
                     if (hasEquals) {
                         results._errors.push(`atomic option '${dashName}' cannot accept a value after '=' (=${value})`);
                         continue
                     } else {
                         value = true;
                     }
+                } else { // requires a value
+                    if (!hasEquals) {
+                        results._errors.push(`value option '${dashName}' requires an '=' sign`);
+                        continue;
+                    }
+                    if (option.required && !value) {
+                        results._errors.push(`value option '${dashName}=' requires a value after '=' sign`);
+                        continue;
+                    }
+                    if (value === "" && option.defaultValue) value = option.defaultValue;
+                    // otherwise, use the value specified `value = words[1];`
                 }
-                let name = option.name;
                 results[name] = results[name] || []; // create an array if it doesn't exist
                 results[name].push(value); // options are always a list
-                console.log({name, value, results})
             } else {
                 // ordered parameter
                 if (nparm < parmlist.length) {
@@ -127,7 +132,7 @@ function trimOption(option) {
     let aliasName;
     dashes = 0;
     let name = option;
-    if (name[0] !== '-') this.die("options must start with a dash (-) " + name);
+    if (name[0] !== '-') throw("options must start with a dash (-) " + name);
     name = name.substring(1); // trim off the leading dash
     dashes++;
     if (name[0] === '-') {
@@ -167,16 +172,18 @@ function parseOptions(optionString) {
     for (let optionItem of optionList) {
         if (!optionItem) continue;
         let hasEquals = optionItem.indexOf("=") !== -1;
+        let isAtomic = !hasEquals;
         let words = optionItem.split("=", 2);
         let optionDefinition = words[0];
         let defaultValue = words[1];
         let { name, required, dashName, aliasName } = trimOption(optionDefinition);
-        if (required && !hasEquals) module.exports.die(`you cannot require option '${dashName}*' without also specifying '='`);
+        if (required && isAtomic) throw(`you cannot require atomic option '${dashName}*'`);
+        if (required && !isAtomic && defaultValue) throw(`you cannot require a value option '${dashName}*' and give it a default value '${defaultValue}'`);
         options[dashName] = {
             name,
             required,
             defaultValue,
-            valueRequired: hasEquals
+            isAtomic: !hasEquals
         };
         if (aliasName) options[aliasName] = options[dashName];
     }
@@ -187,12 +194,13 @@ function parseParms(parmString) {
     let parms = [];
     let parmList = parmString.split(",");
     let cnt = 0;
+    let errors = [];
     for (let parmName of parmList) {
         if (!parmName) continue;
         let words = parmName.split("=", 2);
         let { name, required } = trimParm(words[0]);
         let defaultValue = words[1];
-        if (required && defaultValue) module.exports.die("You cannot do both: require a positional parmeter, and give it a default value: " + parmName);
+        if (required && defaultValue) throw("You cannot do both: require a positional parmeter, and give it a default value: " + parmName);
         let parm = {
             name,
             required,
@@ -205,13 +213,25 @@ function parseParms(parmString) {
 }
 
 function test() {
-    let opts = "--extra-x,-a,-b=,-centry-c=c.default,-d*=_empty_,-e=e.default,-f=,-g*=required";
+    let opts = "-a,-b=,-centry-c=c.default,-d*=,-e=e.default,-f=,--extra-x,";
     let parms = "one,two*,three=three.default,four";
     process.argv = [
         "node",
         "glsprocs.js",
-        "-a", "-b=", "foo", "bar", "-b=funstuff", "-c=", "-c", "-d", "-d=", "-d=d-value", "-f=test", "-x", "-other", "--extra"
+        "-a", "-a", "-b=", "-b=funstuff", "-c", "-c=", "-c=value", "foo", "bar", "-d", "-d=", "-d=d-value", "-f=test", "-x", "-other", "--extra"
     ];
+    try {
+        let options = module.exports.args("-g*=required", parms, false);
+        console.assert(false, "-g*=required did not throw an exception");
+    } catch(ex) {
+        console.assert(ex.toString() === "you cannot require a value option '-g*' and give it a default value 'required'", "-g*=required did not throw the correct exception");
+    }
+    try {
+        let options = module.exports.args("-z*", parms, false);
+        console.assert(false, "-z* did not throw an exception");
+    } catch(ex) {
+        console.assert(ex.toString() === "you cannot require atomic option '-z*'", "-z* did not throw the correct exception");
+    }
     let options = module.exports.args(opts, parms, false);
     console.log(options._errors);
     console.assert(options.files[0] === "foo", "missing first file");
@@ -228,11 +248,11 @@ function test() {
     console.assert(options.b[0] === "", "-b[0] should be the empty string '' ");
     console.assert(options.b[1] === "funstuff", "-b[0] should be funstuff");
     console.assert(options.centry, "-centry (-c) should be specified");
-    console.assert(options.centry[0] === "", "-c[0] should be empty");
-    console.assert(options.centry[1] === "c.default", "-c[1] should be c.default");
+    console.assert(options.centry.length === 2, "-centry (-c) should have 2 values");
+    console.assert(options.centry[0] === "c.default", "-centry[0] have the default value");
+    console.assert(options.centry[1] === "value", "-centry[1] should have the 'value'");
     console.assert(options.d, "-d should be specified at least once");
-    console.assert(options.d[0] === "_empty_", "-d[0] should be _empty_");
-    console.assert(options.d[1] === "", "-d[1] should be ''");
+    console.assert(options.d.length === 1, "-d should be specified at least once");
     console.assert(options.d[2] === "d-value", "-d[d] should be d-value");
     console.assert(options.e === undefined, "-e should not be present");
     console.assert(options.f.length === 1, "-f should be assigned");
@@ -241,15 +261,7 @@ function test() {
     console.assert(options._errors.length === 2, "there should be 2 errors");
     console.assert(options._errors[0] === 'Unknown option: -other', "__errors should report 'Unknown option: -other'");
     console.assert(options._errors[1] === 'Missing required option: -g', "__errors should report 'Missing required option: -g'");
-    console.log("a: " + JSON.stringify(options.a));
-    console.log("b: " + JSON.stringify(options.b));
-    console.log("c: " + JSON.stringify(options.centry));
-    console.log("d: " + JSON.stringify(options.d));
-    console.log("e: " + JSON.stringify(options.e));
-    console.log("f: " + JSON.stringify(options.f));
-    console.log("g: " + JSON.stringify(options.f));
-    console.log("x: " + JSON.stringify(options.x));
-    console.log("extra: " + JSON.stringify(options.extra));
+    console.log(JSON.stringify(options, null, 2));
     console.log(opts);
     console.log(JSON.stringify(process.argv));
 
